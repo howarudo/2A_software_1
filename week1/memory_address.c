1ace3a6020642e8d2dfdcf263a7e79dcda00f997
Howard Tangkulung <howardtangkulung@gmail.com>
2023-10-05 16:15:10 +0900

    checked the memory addresses of ints and chars

---
+++ week1/memory_address.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+// int main() {
+//     int a = 6, b = 8;
+//     printf("a: %p, b: %p\n", &a, &b);
+//     return 0;
+// }
+
+int main() {
+    char a = 6, b = 8;
+    printf("a: %p, b: %p\n", &a, &b);
+    return 0;
+}
